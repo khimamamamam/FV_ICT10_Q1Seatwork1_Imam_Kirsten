ec17916b28541d4fdec8097a84b4439e21be2e37
Kirsten Imam 3 <imam.kj@obmontessori.edu.ph>
2026-09-15 07:15:54 +0800

    ewr

---
+++ main.py
@@ -0,0 +1,17 @@
+from pyscript import display
+#
+
+#a = 0 # integer
+#b = 0.1 # float
+#c = [] #empty list
+
+#display(bool(a))
+#display(bool(b))
+#display(bool(c))
+
+fullname = 'Juan Dela Cruz' #stirng
+
+display(f'Hi! I am {full name} and I am {ag3_s} years old',
+target = "result")
+
+
