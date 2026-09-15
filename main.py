3b9acb6dba73fb66ea0fd4d3620f47a8d22e19fc
Kirsten Imam 3 <imam.kj@obmontessori.edu.ph>
2026-09-15 08:21:58 +0800

    mk

--- main.py
+++ main.py
@@ -10,6 +10,21 @@ from pyscript import display
 #display(bool(c))
 
 fullname = 'Kirsten Heart D. Imam' #stirng
-ag3_s = '16' #integer
+age = 16 #integer
+display(f'Hi! I am {fullname} and I am {age} years old', target = "result")
 
-display(f'Hi! I am {fullname} and I am {ag3_s} years old', target = "result")
+
+h3ight = 144.78
+_countries = {"kazakhstan", "korea", "china"}
+display(f'My height is {h3ight}, The countries I want to visit are {_countries}', target = "result2")
+
+
+others = {
+    "color": "purple",
+    "car_brand": "scooter",
+    "best_friend": "Both of my sisters, Gab, Aeris, and Caitlyn",
+}
+display(others["color"], others["car_brand"], others["best_friend"], target = "result3")
+
+set = (("avocado", "banana", "grapes", "pineapple", "dragonfruit"))
+display(f'My favorite fruits are {set}', target = "result4")
